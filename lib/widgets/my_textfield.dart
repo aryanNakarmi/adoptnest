26dc89360cb44bac99a4ab1dc0f2285bb9793a50
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-04 23:38:30 +0545

    custom textfield

---
+++ lib/widgets/my_textfield.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MyTextfield extends StatelessWidget {
+  
+  final ValueChanged<String> onChanged;
+  final String text;
+  final String hintText;
+  final TextEditingController controller;
+  final bool isPassword;
+
+  const MyTextfield({super.key,
+  required this.onChanged,
+  required this.text, 
+  required this.hintText, 
+  required this.controller,
+  this.isPassword = false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: isPassword,
+
+      decoration: InputDecoration(
+        hintText: hintText,
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+        border: const OutlineInputBorder()
+      ),
+      onChanged: onChanged,
+      validator:(value){
+        if(value!.isEmpty){
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    );
+  }
+}
