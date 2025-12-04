9e9bcb679d54a4c65f247f28247b9084df6bda16
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-04 16:39:54 +0545

    splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,16 +10,16 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  //for splash screen time out
-  // @override
-  // void initState(){
-  //   super.initState();
+  // for splash screen time out
+  @override
+  void initState(){
+    super.initState();
 
-  //   Timer(Duration(seconds: 3),
-  //   (){
-  //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>OnboardingScreen()));
-  //   });
-  // }
+    Timer(Duration(seconds: 3),
+    (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>OnboardingScreen()));
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
