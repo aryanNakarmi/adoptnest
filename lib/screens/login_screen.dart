21a70cce22076eb5f8c5e76c65616eeb62069da9
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-05 00:05:18 +0545

    textfield

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,52 @@
+import 'package:adoptnest/widgets/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 100,),
+            Text(
+              "AdoptNest",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),
+            ), 
+            SizedBox(height: 50,),
+            Text(
+              "Welcome Back!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),
+            ),
+
+            SizedBox(height: 10,),
+            Text("Login to your account",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+
+            SizedBox(height: 20,),
+            MyTextfield(
+              onChanged: (value){
+                //later
+              } ,
+              hintText: "Email", 
+              controller: emailController
+              prefixIcon)
+
+          ],
+        ),
+      )
+    );
   }
 }
--- lib/widgets/my_textfield.dart
+++ lib/widgets/my_textfield.dart
@@ -3,17 +3,18 @@ import 'package:flutter/material.dart';
 class MyTextfield extends StatelessWidget {
   
   final ValueChanged<String> onChanged;
-  final String text;
+
   final String hintText;
   final TextEditingController controller;
   final bool isPassword;
+  final Icon? prefixIcon;
 
   const MyTextfield({super.key,
   required this.onChanged,
-  required this.text, 
   required this.hintText, 
   required this.controller,
-  this.isPassword = false
+  this.isPassword = false,
+  this.prefixIcon
   });
 
   @override
@@ -24,6 +25,7 @@ class MyTextfield extends StatelessWidget {
 
       decoration: InputDecoration(
         hintText: hintText,
+        prefixIcon: prefixIcon,
         contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
         border:  OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
