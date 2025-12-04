17c74801499fbe21409ede1342d386df7d7bb379
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-04 19:41:22 +0545

    images

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -8,35 +8,8 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
-
-  PageController _pageController = PageController();
-  int currentPage = 0;
-  final List<Map<String, String>>onboardingInfo =[
-    {
-      "title": "Welcome to AdoptNest",
-      "subtitle": "Where every stray finds a nest",
-      "image":"assets/images/onboarding3.png"
-    },
-    
-    {
-      "title": "Adopt your new best friend",
-      "subtitle": "Find your perfect companion and give a home to a pet in need.",
-      "image":"assets/images/onboarding1.png"
-    },
-    
-    {
-      "title": "Connect. Adopt. Care",
-      "subtitle": "Our app connects you with a furry buddy to provide them a forever home",
-      "image":"assets/images/onboarding2.png"
-    },
-    
-  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
-      ),
-    );
+    return const Placeholder();
   }
 }
