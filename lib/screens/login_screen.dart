3200f5999f0588468427b66c080237344e2cd520
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-05 11:13:54 +0545

    login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,97 +1,169 @@
-import 'package:adoptnest/screens/home_screen.dart';
 import 'package:adoptnest/screens/signup_screen.dart';
 import 'package:adoptnest/widgets/my_button.dart';
 import 'package:adoptnest/widgets/my_textfield.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
 
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
+
+  final _formKey= GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-
-            const SizedBox(height: 50),
-            //for title and logo
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo1.png', height: 50,),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+          
+              const SizedBox(height: 50),
+              //for title and logo
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo1.png', height: 50,),
+                const Text(
+                "AdoptNest",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+                ],
+              ),
+              
+              const SizedBox(height: 40),
               const Text(
-              "AdoptNest",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-              ],
-            ),
-            
-            const SizedBox(height: 40),
-            const Text(
-              "Welcome Back!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Login to your account",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18, color: Colors.grey.shade600),
-            ),
-            const SizedBox(height: 30),
-            MyTextfield(
-              onChanged: (value) {},
-              hintText: "Email",
+                "Welcome Back!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 24, fontWeight: FontWeight.bold),
+
+              ),
+              
+              const SizedBox(height: 10),
+              Text(
+                "Login to your account",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.grey.shade600),
+              ),
+              const SizedBox(height: 30),
+
+
+//EMAIL
+              TextFormField(
               controller: emailController,
-              prefixIcon: const Icon(Icons.email),
-            ),
-            const SizedBox(height: 20),
-            MyTextfield(
-              onChanged: (value) {},
-              hintText: "Password",
-              controller: passwordController,
-              isPassword: true,
-              prefixIcon: const Icon(Icons.lock),
-            ),
-            const SizedBox(height: 30),
-            MyButton(
-              text: "Login",
-              onPressed: () {
-                String email = emailController.text.trim();
-                String password = passwordController.text.trim();
-                Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
+              
+              decoration: InputDecoration(
+                hintText: "Email",
+                prefixIcon: const Icon(Icons.email),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(color: Color(0xFFFF8C69), width: 2),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) return "Please enter email";
+                if (!value.contains('@')|| !value.contains(".com"))
+                return "Enter a valid email";
+                return null;
               },
             ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Don't have an account?"),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SignupScreen()),
-                    );
-                  },
-                  child: const Text(
-                    "Sign Up",
-                    style: TextStyle(color: Color(0xFFFF8C69)),
-                  ),
+
+//PASSWORD
+            SizedBox(height: 20,),
+             TextFormField(
+              controller: passwordController,
+              
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                prefixIcon: const Icon(Icons.lock),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
                 ),
-              ],
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(color: Colors.grey.shade400),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide(color: Color(0xFFFF8C69), width: 2),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) return "Please enter password";
+                
+                return null;
+              },
             ),
-          ],
+
+
+
+
+//Button
+              const SizedBox(height: 30),
+              MyButton(
+                text: "Login",
+                onPressed: () {
+
+                  if(_formKey.currentState!.validate()){
+                    setState((){
+
+                    
+                    String email = emailController.text.trim();
+                    String password = passwordController.text.trim();
+                    });
+                    
+
+
+                  }
+
+                  // login logic here
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SignupScreen()),
+                      );
+                    },
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(color: Color(0xFFFF8C69)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/my_textfield.dart
+++ lib/widgets/my_textfield.dart
@@ -8,17 +8,14 @@ class MyTextfield extends StatelessWidget {
   final TextEditingController controller;
   final bool isPassword;
   final Icon? prefixIcon;
-  final Color borderColor;
-  final Color enabledBorderColor;
 
   const MyTextfield({super.key,
   required this.onChanged,
   required this.hintText, 
   required this.controller,
   this.isPassword = false,
-  this.prefixIcon,
-  this.borderColor = const Color(0xFFFF8C69),  this.enabledBorderColor= const Color(0xFFFFCBB5),
-
+  this.prefixIcon
+  
   });
 
   @override
@@ -32,14 +29,17 @@ class MyTextfield extends StatelessWidget {
         prefixIcon: prefixIcon,
         contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 18),
 
-          enabledBorder: OutlineInputBorder(
+        border:  OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide(
-            color: enabledBorderColor), // Border color when not focused
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+          borderSide: BorderSide(color: Color.fromARGB(255, 249, 174, 151)), // Border color when not focused
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide(color: borderColor, width: 2), // Border color when focused
+          borderSide: BorderSide(color: Color(0xFFFF8C69), width: 2), // Border color when focused
         ),
 
       ),
