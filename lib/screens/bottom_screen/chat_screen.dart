18c55f825d5bc97cd6cc7b5b4aafd9ff6462420d
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-14 22:03:24 +0545

    chat

---
+++ lib/screens/bottom_screen/chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(child:Center(
+      child: Text("Chat"),
+    ));
+  }
+}
