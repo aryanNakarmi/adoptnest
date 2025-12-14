0146193a332ab9ddbf4db27a808950140f0c6a70
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-14 22:05:46 +0545

    rescue screen

---
+++ lib/screens/bottom_screen/rescue_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RescueScreen extends StatefulWidget {
+  const RescueScreen({super.key});
+
+  @override
+  State<RescueScreen> createState() => _RescueScreenState();
+}
+
+class _RescueScreenState extends State<RescueScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
