35e7794b8b7120cd5d4a68f44c68cacd72f853b3
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-16 20:57:20 +0545

    OpenSans font added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,8 @@
+import 'package:adoptnest/screens/bottom_screen/chat_screen.dart';
+import 'package:adoptnest/screens/bottom_screen/dashboard_screen.dart';
+import 'package:adoptnest/screens/bottom_screen/profile_screen.dart';
+import 'package:adoptnest/screens/bottom_screen/rescue_screen.dart';
+import 'package:adoptnest/screens/bottom_screen/upload_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,25 +13,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    const DashboardScreen(), 
+    const RescueScreen(),
+    const UploadScreen(),
+    const ChatScreen(),
+    const ProfileScreen(),
+    ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
+      body: lstBottomScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.call),label: 'Rescue'),
+        BottomNavigationBarItem(icon: Icon(Icons.add),label: 'Add'),
+        BottomNavigationBarItem(icon: Icon(Icons.chat),label: 'Chat'),
+        BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
+      ],
+
+      currentIndex: _selectedIndex,
+      onTap: (index){
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
       ),
-      body: Center(
-        child: SizedBox(
-          height: 100,
-          width: 200,
-          
-          child: Card(
-          
-             elevation: 5,
-            child: Text("This is Home", style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-        )
-        )  ,
     );
   }
 }
