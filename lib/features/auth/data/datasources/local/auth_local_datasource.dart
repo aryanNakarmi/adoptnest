846b92bae5cb3b155c120285fea038e52c5c5f3a
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-01 21:17:42 +0545

    auth local datasource

---
+++ lib/features/auth/data/datasources/local/auth_local_datasource.dart
@@ -0,0 +1,70 @@
+import 'package:adoptnest/core/services/hive/hive_service.dart';
+import 'package:adoptnest/features/auth/data/datasources/auth_datasource.dart';
+import 'package:adoptnest/features/auth/data/models/auth_hive_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final authLocalDatasourceProvider = Provider<AuthLocalDatasource>((ref){
+
+  final hiveService =ref.watch(hiveServiceProvider);
+  return AuthLocalDatasource(hiveService: hiveService);
+});
+class AuthLocalDatasource implements IAuthDataSource{
+
+  final HiveService _hiveService;
+  AuthLocalDatasource({
+    required HiveService hiveService}): _hiveService =hiveService;
+
+
+  @override
+  Future<AuthHiveModel?> getCurrentUser() async {
+    try {
+      // No persistent current-user tracking implemented yet; return null by default
+      return Future.value(null);
+    } catch (e) {
+      return Future.value(null);
+    }
+  }
+
+  @override
+  Future<bool> isExistingEmail(String email) {
+     try{
+    final exists = _hiveService.isExistingEmail(email);
+    return Future.value(exists);
+   }catch(e){
+    return Future.value(false);
+   }
+  }
+
+  @override
+  Future<AuthHiveModel?> login(String email, String password) async {
+     try{
+    final user = await _hiveService.loginUser(email, password);
+    return Future.value(user);
+   }catch(e){
+    return Future.value(null);
+   }
+   
+  }
+
+  @override
+  Future<bool> logout() async{
+   try{
+   await _hiveService.logoutUser();
+    return Future.value(true);
+   }catch(e){
+    return Future.value(false);
+   }
+  }
+
+  @override
+  Future<bool> register(AuthHiveModel model) async {
+   try{
+    await _hiveService.registerUser(model);
+    return Future.value(true);
+
+   }catch(e){
+    return Future.value(false);
+   }
+  }
+}
