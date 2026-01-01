108eee44408e67dea7da2b390d85650166e35223
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-01 22:18:13 +0545

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:adoptnest/app/app.dart';
+import 'package:adoptnest/core/services/hive/hive_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await HiveService().init();
+  runApp(const ProviderScope(child: App()));
 }
