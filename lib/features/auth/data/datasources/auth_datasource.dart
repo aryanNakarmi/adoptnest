081a6b2339f7982256fed9f2c3b774a90204c8a6
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-01 21:03:56 +0545

    auth datasource

---
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -0,0 +1,14 @@
+
+import 'package:adoptnest/features/auth/data/models/auth_hive_model.dart';
+
+abstract interface class IAuthDataSource{
+  
+  Future<bool> register(AuthHiveModel model);
+  Future<AuthHiveModel?> login(String email, String password);
+  Future<AuthHiveModel?> getCurrentUser();
+  Future<bool> logout();
+
+  //get email exists
+  Future<bool> isExistingEmail(String email);
+
+}
