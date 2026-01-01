0b1e555d1bc139aab02ddb8cdeb49e6ee27e1b9d
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-01 20:20:32 +0545

    Hive table constant created

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,9 @@
+class HiveTableConstant {
+  HiveTableConstant._();
+
+  //Database name
+  static const String dbName = 'adoptnest_db';
+
+  static const int authTypeId = 0;
+  static const String authTable ='auth_table';
+}
