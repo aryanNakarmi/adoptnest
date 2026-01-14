952eed0632d9348d5c04d3f31867d7f42cd29745
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-14 19:22:16 +0545

    getUser usecase updated

---
+++ lib/features/auth/domain/usecases/get_current_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:adoptnest/core/error/failures.dart';
+import 'package:adoptnest/core/usecases/app_usecase.dart';
+import 'package:adoptnest/features/auth/data/repositories/auth_repository.dart';
+import 'package:adoptnest/features/auth/domain/entities/auth_entity.dart';
+import 'package:adoptnest/features/auth/domain/repositories/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Create Provider
+final getCurrentUserUsecaseProvider = Provider<GetCurrentUserUsecase>((ref) {
+  final authRepository = ref.read(AuthRepositoryProvider);
+  return GetCurrentUserUsecase(authRepository: authRepository);
+});
+
+class GetCurrentUserUsecase implements UsecaseWithoutParams<AuthEntity> {
+  final IAuthRepository _authRepository;
+
+  GetCurrentUserUsecase({required IAuthRepository authRepository})
+    : _authRepository = authRepository;
+
+  @override
+  Future<Either<Failure, AuthEntity>> call() {
+    return _authRepository.getCurrentUser();
+  }
+}
