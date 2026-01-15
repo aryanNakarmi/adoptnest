ec58f402502e89f703d0b9462c331e10fd0a54de
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-15 18:24:24 +0545

    auth remote datasource

---
+++ lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
@@ -0,0 +1,67 @@
+import 'package:adoptnest/core/api/api_client.dart';
+import 'package:adoptnest/core/api/api_endpoints.dart';
+import 'package:adoptnest/core/services/storage/user_session_service.dart';
+import 'package:adoptnest/features/auth/data/datasources/auth_datasource.dart';
+import 'package:adoptnest/features/auth/data/models/auth_api_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Provider
+final authRemoteDatasourceProvider = Provider<AuthRemoteDatasource>((ref) {
+  return AuthRemoteDatasource(
+    apiClient: ref.read(apiClientProvider),
+    userSessionService: ref.read(userSessionServiceProvider),
+  );
+});
+
+class AuthRemoteDatasource implements IAuthRemoteDataSource {
+  final ApiClient _apiClient;
+  final UserSessionService _userSessionService;
+  AuthRemoteDatasource({
+    required ApiClient apiClient,
+    required UserSessionService userSessionService,
+  }) : _apiClient = apiClient,
+       _userSessionService = userSessionService;
+
+  @override
+  Future<AuthApiModel?> getUserById(String authId) {
+    // TODO: implement getUserById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<AuthApiModel?> login(String email, String password) async {
+    final response = await _apiClient.post(
+      ApiEndpoints.login,
+      data: {'email': email, 'password': password},
+    );
+
+    if (response.data['success'] == true) {
+      final data = response.data['data'] as Map<String, dynamic>;
+      final user = AuthApiModel.fromJson(data);
+
+      //Save user session
+      await _userSessionService.saveUserSession(
+        userId: user.id!,
+        email: user.email,
+        fullName: user.fullName,
+        username: user.username,
+      );
+      return user;
+    }
+    return null;
+  }
+
+  @override
+  Future<AuthApiModel> register(AuthApiModel user) async {
+    final response = await _apiClient.post(
+      ApiEndpoints.register,
+      data: user.toJson(),
+    );
+    if (response.data['success'] == true) {
+      final data = response.data['data'] as Map<String, dynamic>;
+      final registeredUser = AuthApiModel.fromJson(data);
+      return registeredUser;
+    }
+    return user;
+  }
+}
