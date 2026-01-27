78aab3e162d23ffb1a70a2905263f972f443897c
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-27 21:59:12 +0545

    update status usecase

---
+++ lib/features/report_animals/domain/usecases/update_report_status_usecase.dart
@@ -0,0 +1,37 @@
+import 'package:adoptnest/core/usecases/app_usecase.dart';
+import 'package:adoptnest/features/report_animals/data/repositories/animal_report_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:adoptnest/core/error/failures.dart';
+import '../repositories/animal_report_repository.dart';
+import '../entities/animal_report_entity.dart';
+
+class UpdateReportStatusParams extends Equatable {
+  final String reportId;
+  final String newStatus;
+
+  const UpdateReportStatusParams({required this.reportId, required this.newStatus});
+
+  @override
+  List<Object?> get props => [reportId, newStatus];
+}
+
+final updateReportStatusUsecaseProvider =
+    Provider<UpdateReportStatusUsecase>((ref) {
+  final repository = ref.read(animalReportRepositoryProvider);
+  return UpdateReportStatusUsecase(repository: repository);
+});
+
+class UpdateReportStatusUsecase
+    implements UsecaseWithParams<AnimalReportEntity, UpdateReportStatusParams> {
+  final IAnimalReportRepository _repository;
+
+  UpdateReportStatusUsecase({required IAnimalReportRepository repository})
+      : _repository = repository;
+
+  @override
+  Future<Either<Failure, AnimalReportEntity>> call(UpdateReportStatusParams params) {
+    return _repository.updateReportStatus(params.reportId, params.newStatus);
+  }
+}
