b5e01279ba1a4cf23680e66e2cb6fbe673e842d0
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-27 21:44:12 +0545

    get my report usecase

---
+++ lib/features/report_animals/domain/usecases/get_my_report_usecase.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:adoptnest/core/error/failures.dart';
+import 'package:adoptnest/core/usecases/app_usecases.dart';
+import '../repositories/animal_report_repository.dart';
+import '../entities/animal_report_entity.dart';
+
+class GetMyReportsParams extends Equatable {
+  final String userId;
+
+  const GetMyReportsParams({required this.userId});
+
+  @override
+  List<Object?> get props => [userId];
+}
+
+final getMyReportsUsecaseProvider =
+    Provider<GetMyReportsUsecase>((ref) {
+  final repository = ref.read(animalReportRepositoryProvider);
+  return GetMyReportsUsecase(repository: repository);
+});
+
+class GetMyReportsUsecase
+    implements UsecaseWithParams<List<AnimalReportEntity>, GetMyReportsParams> {
+  final IAnimalReportRepository _repository;
+
+  GetMyReportsUsecase({required IAnimalReportRepository repository})
+      : _repository = repository;
+
+  @override
+  Future<Either<Failure, List<AnimalReportEntity>>> call(
+      GetMyReportsParams params) {
+    return _repository.getMyReports(params.userId);
+  }
+}
