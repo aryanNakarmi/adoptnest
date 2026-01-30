d4f43605f37f91f83a61925edf955e7b6fb53101
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-30 21:00:23 +0545

    token service to login

--- lib/core/services/storage/token_service.dart
+++ lib/core/services/storage/token_service.dart
@@ -1,3 +1,4 @@
+import 'package:adoptnest/core/services/storage/user_session_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
--- lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/remote/auth_remote_datasource.dart
@@ -1,5 +1,6 @@
 import 'package:adoptnest/core/api/api_client.dart';
 import 'package:adoptnest/core/api/api_endpoints.dart';
+import 'package:adoptnest/core/services/storage/token_service.dart';
 import 'package:adoptnest/core/services/storage/user_session_service.dart';
 import 'package:adoptnest/features/auth/data/datasources/auth_datasource.dart';
 import 'package:adoptnest/features/auth/data/models/auth_api_model.dart';
@@ -9,18 +10,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final authRemoteDatasourceProvider = Provider<AuthRemoteDatasource>((ref) {
   return AuthRemoteDatasource(
     apiClient: ref.read(apiClientProvider),
-    userSessionService: ref.read(userSessionServiceProvider),
+    userSessionService: ref.read(userSessionServiceProvider), 
+    tokenService: ref.read(tokenServiceProvider),
   );
 });
 
 class AuthRemoteDatasource implements IAuthRemoteDataSource {
   final ApiClient _apiClient;
   final UserSessionService _userSessionService;
+  final TokenService _tokenService;
+
   AuthRemoteDatasource({
     required ApiClient apiClient,
     required UserSessionService userSessionService,
+    required TokenService tokenService
   }) : _apiClient = apiClient,
-       _userSessionService = userSessionService;
+       _userSessionService = userSessionService,
+       _tokenService = tokenService;
 
   @override
   Future<AuthApiModel?> getUserById(String authId) {
